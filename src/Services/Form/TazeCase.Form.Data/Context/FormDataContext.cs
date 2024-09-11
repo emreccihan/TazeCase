c6ad3044e0bf3e2d1ba752a4214cabbcb43d02f3
emreccihan <emreccihan1@gmail.com>
2024-09-11 20:50:33 +0300

    feat: Configure DbContext and add initial migrations

--- src/Services/Form/TazeCase.Form.Data/Context/FormDataContext.cs
+++ src/Services/Form/TazeCase.Form.Data/Context/FormDataContext.cs
@@ -13,5 +13,38 @@ namespace TazeCase.Form.Data.Context
         {
 
         }
+
+        public DbSet<Core.Entities.Form> Forms { get; set; }
+        public DbSet<Core.Entities.FormData> FormData { get; set; }
+        public DbSet<Core.Entities.FormField> FormFields { get; set; }
+        public DbSet<Core.Entities.FormData> FormDatas { get; set; }
+        public DbSet<Core.Entities.FormDataValue> FormDataValues { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Core.Entities.Form>(entity =>
+            {
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+
+            modelBuilder.Entity<Core.Entities.FormData>(entity =>
+            {
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+            modelBuilder.Entity<Core.Entities.FormField>(entity =>
+            {
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+            modelBuilder.Entity<Core.Entities.FormData>(entity =>
+            {
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+            modelBuilder.Entity<Core.Entities.FormDataValue>(entity =>
+            {
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+        }
     }
 }
---
+++ src/Services/Form/TazeCase.Form.Data/Migrations/20240911174351_AddFormTables.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TazeCase.Form.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFormTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FormName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FormId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormData_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormFields",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FieldName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FieldType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsRequired = table.Column<bool>(type: "bit", nullable: false),
+                    FormId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormFields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormFields_Forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "Forms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormDataValues",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FieldName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FormDataId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormDataValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FormDataValues_FormData_FormDataId",
+                        column: x => x.FormDataId,
+                        principalTable: "FormData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormData_FormId",
+                table: "FormData",
+                column: "FormId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormDataValues_FormDataId",
+                table: "FormDataValues",
+                column: "FormDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FormFields_FormId",
+                table: "FormFields",
+                column: "FormId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormDataValues");
+
+            migrationBuilder.DropTable(
+                name: "FormFields");
+
+            migrationBuilder.DropTable(
+                name: "FormData");
+
+            migrationBuilder.DropTable(
+                name: "Forms");
+        }
+    }
+}
